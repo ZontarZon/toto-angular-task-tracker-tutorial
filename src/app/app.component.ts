54dfd619b25d0daf710ae8826d049ab886c3d455
Thorya <thoryagudrun@gmail.com>
2023-09-10 15:59:52 -0400

    Cleared default css, added css from tutorial, made a header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // The tag you're gonna use to embed your component. This one is embedded into index.html.
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'toto-angular-task-tracker-tutorial';
+  title: string = 'Toto Task Tracker';
 }
